fe184d4342b856524cbc2b2137f1b9fa4953d393
alfaruq1054 <azzamalfaruq105@gmail.com>
2022-10-21 09:54:59 +0700

    add lagi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,36 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    {{-- Tambahan template --}}
+
+    {{-- <title>@yield('title')</title> --}}
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('template/vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/vendors/css/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('template/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="js/select.dataTables.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('template/css/vertical-layout-light/style.css') }}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('template/images/favicon.png') }}" />
+
+    {{--Akhir Tambahan template --}}
+
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
+    @yield('css')
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -70,11 +93,35 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
+        @yield('content')
 
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    {{-- Tambah JS --}}
+    <!-- plugins:js -->
+    <script src="{{ asset('template/vendors/js/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="{{ asset('template/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('template/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('template/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('template/js/dataTables.select.min.js') }}"></script>
+
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{ asset('template/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('template/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('template/js/template.js') }}"></script>
+    <script src="{{ asset('template/js/settings.js') }}"></script>
+    <script src="{{ asset('template/js/todolist.js') }}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{ asset('template/js/dashboard.js') }}"></script>
+    <script src="{{ asset('template/js/Chart.roundedBarCharts.js') }}"></script>
+    <!-- End custom js for this page-->
+    @yield('js')
+    {{--akhir Tambah JS --}}
 </body>
 </html>
