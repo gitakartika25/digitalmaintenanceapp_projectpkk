29a0991a6d2f97da3724154fcb334fe7491860b0
alfaruq1054 <azzamalfaruq105@gmail.com>
2022-10-21 10:03:13 +0700

    Add

--- resources/views/master/index_user.blade.php
+++ resources/views/master/index_user.blade.php
@@ -202,6 +202,7 @@
     </footer>
   </div>
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <script src="{{ asset('template_user/js/jquery-3.3.1.min.js') }}"></script>
   <script src="{{ asset('template_user/js/jquery-ui.js') }}"></script>
   <script src="{{ asset('template_user/js/popper.min.js') }}"></script>
