1c0e3f79a3832dbe648a81c694233c42ba4a1f53
alfaruq1054 <azzamalfaruq105@gmail.com>
2022-10-21 10:09:18 +0700

    minor

--- resources/views/master/index.blade.php
+++ resources/views/master/index.blade.php
@@ -109,7 +109,7 @@
               <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i class="ti-power-off text-primary"></i>
                 {{ __('Logout') }}
-               </a>
+              </a>
            
                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                    @csrf
