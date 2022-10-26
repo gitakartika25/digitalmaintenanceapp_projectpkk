16cf60fe85e8f1953ec44c329ced0dbb79df62dc
dhomanhuri <dhomanhuri@gmail.com>
2022-10-26 14:10:18 +0700

    migration done

---
+++ app/Http/Controllers/ReturnsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\returns;
+use Illuminate\Http\Request;
+
+class ReturnsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\returns  $returns
+     * @return \Illuminate\Http\Response
+     */
+    public function show(returns $returns)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\returns  $returns
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(returns $returns)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\returns  $returns
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, returns $returns)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\returns  $returns
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(returns $returns)
+    {
+        //
+    }
+}
