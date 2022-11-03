e0fe7c931d6c7ab77fbd6ed60df0def956a8a47f
AryaPutra21-arch <onepiecenw12@gmail.com>
2022-11-03 15:57:35 +0700

    ediat dikit

--- resources/views/users/history.blade.php
+++ resources/views/users/history.blade.php
@@ -29,30 +29,19 @@
                 <td class="text-danger"> 28.76% <i class="ti-arrow-down"></i></td>
                 <td><label class="badge badge-danger">Pending</label></td>
               </tr>
+
+              {{-- @foreach ($data as $d)
               <tr>
-                <td>Messsy</td>
-                <td>Flash</td>
-                <td class="text-danger"> 21.06% <i class="ti-arrow-down"></i></td>
-                <td><label class="badge badge-warning">In progress</label></td>
-              </tr>
-              <tr>
-                <td>John</td>
-                <td>Premier</td>
-                <td class="text-danger"> 35.00% <i class="ti-arrow-down"></i></td>
-                <td><label class="badge badge-info">Fixed</label></td>
-              </tr>
-              <tr>
-                <td>Peter</td>
-                <td>After effects</td>
-                <td class="text-success"> 82.00% <i class="ti-arrow-up"></i></td>
-                <td><label class="badge badge-success">Completed</label></td>
-              </tr>
-              <tr>
-                <td>Dave</td>
-                <td>53275535</td>
-                <td class="text-success"> 98.05% <i class="ti-arrow-up"></i></td>
-                <td><label class="badge badge-warning">In progress</label></td>
-              </tr>
+                <td> <img src="{{ asset('storage/'.$d->image) }}" alt="">
+                
+                </td>
+                <td> {{ $d->id }}</td>
+                <td> {{ $d->image }}</td>
+                <td> {{ $d->product }}</td>
+                <td> {{ $d->price }}</td>
+                <td> {{ $d->quantity }}</td>
+                <td> {{ $d->total }}</td>
+              </tr> --}}
             </tbody>
           </table>
         </div>
